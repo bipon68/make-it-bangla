9ae5bed3822d2b41f84b22f87ce51102c1fc7cc9
Bipon Biswas <bipon@alemcloud.com>
2018-04-02 18:57:29 +0600

    crud part 2

---
+++ crud/part-2/components/Portfolio.js
@@ -0,0 +1,19 @@
+class Portfolio extends React.Component {
+  render() {
+    //console.log(this.props);
+    return (
+      <div className="Portfolio">
+        <div className="image-wrapper">
+          <img width="450" height="250" src={this.props.post.image} alt="portfolio" />
+        </div>
+        <div className="content-area">
+          <h2>{this.props.post.title}</h2>
+          <p>{this.props.post.text}</p>
+          <button onClick={()=>this.props.handleDelete()}>Delete</button>
+          <button onClick={()=>this.props.handleSingleView()}>View</button>
+        </div>
+
+      </div>
+    )
+  }
+}
